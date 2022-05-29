a5be978aa4a8bd3e91e2fb3d894458fa3de42b32
Vania Bogdan <bogdan.vania@gmail.com>
2022-05-29 20:30:22 +0200

    Delete task-07.js

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector("#font-size-control");
-const span = document.querySelector("#text");
-console.log(span);
-
-input.addEventListener("input", (event) => {
-  span.style.fontSize = `${event.currentTarget.value}px`;
-});
