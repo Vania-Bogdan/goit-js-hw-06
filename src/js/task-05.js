8c203e2dbe557a6ae708d4ff29693a8b035a5008
Vania Bogdan <bogdan.vania@gmail.com>
2022-05-29 20:30:17 +0200

    Delete task-05.js

--- src/js/task-05.js
+++
@@ -1,8 +0,0 @@
-const userName = document.querySelector("#name-input")
-const namePlace = document.querySelector("#name-output")
-userName.addEventListener("input", (event) => {
-    namePlace.textContent = event.currentTarget.value
-    if (event.currentTarget.value === "") {
-        namePlace.textContent = "Anonymous"
-    }
-});
