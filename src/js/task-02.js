09f93ccd2362b96cafddb02072fd02d2be17d9d4
Vania Bogdan <bogdan.vania@gmail.com>
2022-05-29 12:55:42 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientsLi = [];
+ingredients.map((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  ingredientsLi.push(liElement);
+});
+list.append(...ingredientsLi);
